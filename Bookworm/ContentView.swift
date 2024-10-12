6e8e27fae7d26964f5bb76596cd2a000b19538df
sxmtee <somtoo76@gmail.com>
2024-10-12 17:03:09 +0100

    TextEditor

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -7,15 +7,39 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button(title) {
+            isOn.toggle()
         }
         .padding()
+        .background(
+            LinearGradient(
+                colors: isOn ? onColors : offColors,
+                startPoint: .top,
+                endPoint: .bottom
+            )
+        )
+        .foregroundColor(.white)
+        .clipShape(.capsule)
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
     }
 }
 
---
+++ Bookworm/TextEditor.swift
@@ -0,0 +1,18 @@
+//
+//  TextEditor.swift
+//  Bookworm
+//
+//  Created by mac on 12/10/2024.
+//
+
+import SwiftUI
+
+struct TextEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextEditor()
+}
